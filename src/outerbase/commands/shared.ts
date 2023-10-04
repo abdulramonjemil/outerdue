0d12f4b1193a4d192a149355c60af98d10d6108d
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2023-10-04 10:41:51 +0100

    Change the origin in sample command interface

--- src/outerbase/commands/shared.ts
+++ src/outerbase/commands/shared.ts
@@ -1,7 +1,7 @@
 import { defineCommandInterface } from "@/outerbase/system/command"
 
 export const SampleCommandInterface = defineCommandInterface({
-  origin: "https://tired-violet.cmd.outerbase.io",
+  origin: "https://some-prefix.cmd.outerbase.io",
   prefix: null,
   namespaces: ["access", "test"],
   methods: ["GET", "POST", "PUT"]
