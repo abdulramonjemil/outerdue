7a1141dd6438ea2756d12d8c5c6546881531cc75
Abdulramon Jemil <141630049+abdulramon-jemil@users.noreply.github.com>
2023-10-04 08:24:42 +0100

    Remove unused namespace

--- src/outerbase/commands/shared.ts
+++ src/outerbase/commands/shared.ts
@@ -3,6 +3,6 @@ import { defineCommandsInterface } from "@/outerbase/system/command/shared"
 export const SampleCommandsInterface = defineCommandsInterface({
   origin: "https://tired-violet.cmd.outerbase.io",
   prefix: null,
-  namespaces: ["access", "sync", "test"],
+  namespaces: ["access", "test"],
   methods: ["GET", "POST", "PUT"]
 })
